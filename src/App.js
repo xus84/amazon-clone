ccd11ed706b8c29d568397f5cd809b8774bc9691
Sergio Marcilla <sermara70@gmail.com>
2020-11-02 23:53:02 +0100

    Adds logo inner header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 import Home from "./Home";
+import Header from "./Header"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/">
+            <Header />
             <Home/>
           </Route>
         </Switch>
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css"
+import SearchIcon   from "@material-ui/icons/Search"
+import ShoppingBasketInfo from "@material-ui/icons/ShoppingBasket"
+
+export default function Header() {
+  return (
+    <div className="header"> 
+        <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+        
+
+    </div>
+  );
+}
