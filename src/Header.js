733077a8776b327a83f05f7eb1f011baf6af2eee
Sergio Marcilla <sermara70@gmail.com>
2020-11-03 00:47:38 +0100

    Adds HeaderNav component without css style

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import "./Header.css"
 import SearchIcon   from "@material-ui/icons/Search"
-import ShoppingBasketInfo from "@material-ui/icons/ShoppingBasket"
+import HeaderNav from "./HeaderNav"
+
+const getOptions = () => ({
+      headerOptions:[
+            {classOne:"header__optionLineOne", classTwo:"header__optionLineTwo", textOne:"Hello Guest", textTwo:"Sing In"},
+            {classOne:"header__optionLineOne", classTwo:"header__optionLineTwo", textOne:"Returns", textTwo:"& Orders"},
+            {classOne:"header__optionLineOne", classTwo:"header__optionLineTwo", textOne:"Your", textTwo:"Prime"},
+          ], 
+      headerOptionBasket:{classOne:"header__optionLineOne", classTwo:"header__optionLineTwo"}})
 
 export default function Header() {
+  const {headerOptions, headerOptionBasket} = getOptions();
   return (
     <div className="header"> 
         <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
@@ -11,7 +20,7 @@ export default function Header() {
           <input className="header__searchInput"/>
           <SearchIcon className="header_searchIcon"/>
         </div>
-
+        <HeaderNav {...{headerOptions, headerOptionBasket}}/>
     </div>
   );
 }
---
+++ src/HeaderNav.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ShoppingBasketInfo from "@material-ui/icons/ShoppingBasket"
+
+const HeaderOption = ({op, i}) => 
+    <div key = {i} className="header__option">
+        <span className={`"${op.classOne}"`}>{op.textOne}</span>
+        <span className={`"${op.classTwo}"`}>{op.textTwo}</span>
+    </div>
+
+const HeaderOptionBasket = ({op}) => 
+    <div className="header__optionBasket">
+        <ShoppingBasketInfo />
+        <span className={`"${op.classTwo} ${op.classCount}"`}>0</span>
+    </div>
+
+const HeaderNav = props => {
+    const {headerOptions, headerOptionBasket} = props;
+
+  return (
+    <div className="header__nav">
+        {headerOptions.map((op, index)=><HeaderOption {...{op, index}}/>)}
+        <HeaderOptionBasket {...{op:headerOptionBasket}} />        
+    </div>
+  )
+}
+
+export default HeaderNav 
