22530cd71004d2f9aca48ddfea581e5e5a9f2ef4
xus84 <g_garsan@hotmail.com>
2020-11-22 23:55:06 +0100

    cambios homepage y checkoutpage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Header from "./Header";
 import './App.css';
 import Home from "./Home";
-import Header from "./Header"
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
-    <div className="app">
-      <Router>
+    <Router>
+      <div className="app">
+      <Header />
+     
         <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
           <Route path="/">
-            <Header />
             <Home/>
           </Route>
         </Switch>
-      </Router>
-    </div>
+     </div>
+    </Router>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,49 +1,55 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
-import HeaderNav from "./HeaderNav";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
 
-const getOptions = () => ({
-  headerOptions: [
-    {
-      classOne: "header__optionLineOne",
-      classTwo: "header__optionLineTwo",
-      textOne: "Hello Guest",
-      textTwo: "Sing In",
-    },
-    {
-      classOne: "header__optionLineOne",
-      classTwo: "header__optionLineTwo",
-      textOne: "Returns",
-      textTwo: "& Orders",
-    },
-    {
-      classOne: "header__optionLineOne",
-      classTwo: "header__optionLineTwo",
-      textOne: "Your",
-      textTwo: "Prime",
-    },
-  ],
-  headerOptionBasket: {
-    classTwo: "header__optionLineTwo",
-    classCount: "header__basketCount",
-  },
-});
-
-export default function Header() {
-  const { headerOptions, headerOptionBasket } = getOptions();
+function Header() {  
   return (
     <div className="header">
+      <Link to="/">
       <img
-        className="header__logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt=""
-      />
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
+        
+      
       <div className="header__search">
-        <input className="header__searchInput" />
+        <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
       </div>
-      <HeaderNav {...{ headerOptions, headerOptionBasket }} />
+
+      <div className="header__nav">
+        
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello Guest</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        
+
+        
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        
+        
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+          <Link to="/checkout">
+           <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">0</span>
+           </div>
+          </Link>
+      </div>
     </div>
   );
 }
+
+export default Header;
